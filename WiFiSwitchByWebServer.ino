1af0abf6edc97358592e0775c2b2906589162176
Sam Sharing <Sam3258@users.noreply.github.com>
2020-07-17 15:51:44 +0800

    Add SSD1315 OLED display for IP show-up

--- WiFiSwitchByWebServer.ino
+++ WiFiSwitchByWebServer.ino
@@ -11,18 +11,28 @@
   History   :
   Date			Author		Ref		Revision
   20200611		Sam			0.01	Initial version
+  20200611		Sam			0.02	Add SD1315 display for IP show-up
 */
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-//#include <Hash.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
 
 // Replace with your WiFi SSID & Password
-const char* ssid = "YourWiFi";
-const char* password = "password";
+const char* ssid = "Your SSID";
+const char* password = "Password";
 
+IPAddress 	ip;
+char 	 	printBuf[64];
 
 // Replace with light control pin as your hardware
 uint8_t lightCtrlPin = D5;			// original WiFi Switch uses the GPIO13 / D7 pin
@@ -30,14 +40,20 @@ bool lightStatus = LOW;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+// Create object SSD1315
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
 
 	Serial.begin(115200);
+	WiFi.setSleepMode(WIFI_NONE_SLEEP);
 
 	// Initial the GPIO pinMode and state
 	pinMode(lightCtrlPin, OUTPUT);
 	digitalWrite(lightCtrlPin, lightStatus);
+
+	// Start SSD1315
+	u8g2.begin();
 
 	// Show-up WiFi info
 	Serial.println("WiFi is connecting to ");
@@ -56,7 +72,12 @@ void setup() {
 	Serial.println("");
 	Serial.println("WiFi connected.");
 	Serial.println("IP address: ");
-	Serial.println(WiFi.localIP());
+//	Serial.println(WiFi.localIP());
+	IPAddress ip = WiFi.localIP();
+	Serial.println(ip);
+
+	memset(printBuf, 0, sizeof(printBuf));
+	sprintf(printBuf, "%u.%u.%u.%u", ip[0], ip[1], ip[2], ip[3]);
 
 	// Define handles
 	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
@@ -82,6 +103,13 @@ void setup() {
 
 	server.begin();
 	Serial.println("Start WebServer");
+
+	u8g2.clearBuffer();                   		// clear the internal memory
+	u8g2.setFont(u8g2_font_ncenB14_tr);			// setup font
+	u8g2.drawStr(12, 24, "WiFi Switch");
+	u8g2.setFont(u8g2_font_ncenB10_tr);
+	u8g2.drawStr(24, 56, printBuf);
+	u8g2.sendBuffer();                    		// transfer internal memory to the display
 }
 
 void loop() {
@@ -92,6 +120,7 @@ void loop() {
 	else {
 		digitalWrite(lightCtrlPin, LOW);
 	}
+	delay(500);
 }
 
 String SendHTML(uint8_t lightState) {
@@ -110,14 +139,13 @@ String SendHTML(uint8_t lightState) {
 	ptr +="</style>\n";
 	ptr +="</head>\n";
 	ptr +="<body>\n";
-	ptr +="<h1>WiFi Switch Implementation</h1>\n";
-    ptr +="<h3>Using AsyncWeb Sever Library</h3>\n";
+	ptr +="<h1>WiFi Switch</h1>\n";
+    ptr +="<h3>Implement by Async WebSever</h3>\n";
 
     if ( lightState )
     	{ptr +="<p>Light Status: ON</p><a class=\"button button-off\" href=\"/lightoff\">OFF</a>\n";}
     else
     	{ptr +="<p>Light Status: OFF</p><a class=\"button button-on\" href=\"/lighton\">ON</a>\n";}
-
 
     ptr +="</body>\n";
     ptr +="</html>\n";
